0be2d9947195a1ceb6c0e339724d9f2bb5bc24ec
bbaxa16 <brooke769@gmail.com>
2018-01-23 00:48:29 -0700

    fixed server

--- server.js
+++ server.js
@@ -1,5 +1,15 @@
-var mongoUri = process.env.MONGODB_URI
-const port = process.env.PORT || 3000;
+const express       = require('express');
+const bodyParser    = require('body-parser');
+const mongoose      = require('mongoose');
+const app           = express();
+const router        = express.Router();
+const port = process.env.PORT || 3000
+//middleware
+app.use(express.static('public'));
+app.use(bodyParser.json());
+
+const mongoUri = process.env.MONGODB_URI
+
 mongoose.connect(mongoUri);
 
 app.listen(port)
